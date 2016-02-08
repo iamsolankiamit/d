753f820064d49ae927b5a9d5ad50907cdb828be1
Yuvraj Chouhan <dilip.chouhan7@gmail.com>
2016-02-09 01:18:08 +0530

    removed frontend

--- frontend/app/models/video.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  movie_file_name: DS.attr('string'),
-  movie_content_type: DS.attr('string'),
-  playlistRelation: DS.belongsTo('playlist')
-});
--- frontend/app/routes/playlist.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-	renderTemplate: function() {
-      this.render('playlist', { into: 'application' });
-  },
-	model: function(params) {
-    	return this.store.find('playlist', params.id);}
-});
